df2ac070429a0582a3194c74f85cfcff92c9a589
matheus-sousa-nascimento <143366856+matheus-sousa-nascimento@users.noreply.github.com>
2025-03-22 23:56:27 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace exercicio08
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite o numero para saber a área e o perímetro do quadrado: ");
+            int num = int.Parse(Console.ReadLine());
+            int area = num * num;
+            int perimetro = num * 4;
+            Console.WriteLine("A área do quadrado é " + area + " e o perímetro do quadrado é " + perimetro);
+        }
+    }
+}
